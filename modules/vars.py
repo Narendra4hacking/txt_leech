5a5423d3794d8f589ea93612d846e1706baf4609
Jaat bhae <154989744+Narendra4hacking@users.noreply.github.com>
2024-05-06 08:08:06 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "25851293"
 api_hash = "e36229fe99be858c25783f6ac82cc1bc"
-bot_token = "7163878711:AAG6BvNJxcAb5zc9MGTZ-LXkOI7mNRgqd-A"
+bot_token = "7165480371:AAEv-HQinJbXy5uleG7gzPJlzf0zMoPtLfY"
